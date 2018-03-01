3217f6472c392cc548875c928854c3152571ed93
Dennis T <den_tra1@hotmail.com>
2018-02-28 20:09:18 -0500

    Refactored scrip.js, moved functons into onload

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -21,44 +21,45 @@
                 modal.setAttribute('data-target', '#myModal');
             });
         }
-    });
-    // Extremely useful source
-    //https://stackoverflow.com/questions/31060642/preload-multiple-audio-files
 
-    var audioFiles = [
-        new Audio("/file/808bd01.mp3"),
-        new Audio("/file/808bd09.mp3"),
-        new Audio("/file/808sd02.mp3")
-    ];
+        // Extremely useful source
+        //https://stackoverflow.com/questions/31060642/preload-multiple-audio-files
 
-    /*// How to loop but hardcoded
-    audioFiles[0].addEventListener('ended', function(){
-        audioFiles[1].play();
-    });
-    audioFiles[1].addEventListener('ended', function(){
-        audioFiles[2].play();
-    });
-    audioFiles[2].addEventListener('ended', function(){
-        audioFiles[0].play();
-    });*/
+        var audioFiles = [
+            new Audio("/file/808bd01.mp3"),
+            new Audio("/file/808bd09.mp3"),
+            new Audio("/file/808sd02.mp3")
+        ];
 
-    document.getElementById('test').addEventListener('click', function(e){
-        /*// This plays all sounds at once.
-        for (var i in audioFiles) {
-            audioFiles[i].play();
-        }*/
-
-        audioFiles[0].play();
-    });
-
-    document.querySelector('#upload_form').addEventListener('submit', function(e){
-        e.preventDefault();
-        var file = document.getElementById('file').files[0];
-        api.upload(file, function(err, file){
-            if (err) return alert(err);
-            console.log("Stored in DB");
+        /*// How to loop but hardcoded
+        audioFiles[0].addEventListener('ended', function(){
+            audioFiles[1].play();
         });
-        document.querySelector('#upload_form').reset();
-    });
+        audioFiles[1].addEventListener('ended', function(){
+            audioFiles[2].play();
+        });
+        audioFiles[2].addEventListener('ended', function(){
+            audioFiles[0].play();
+        });*/
 
+        document.getElementById('test').addEventListener('click', function(e){
+            /*// This plays all sounds at once.
+            for (var i in audioFiles) {
+                audioFiles[i].play();
+            }*/
+
+            audioFiles[0].play();
+        });
+
+        document.querySelector('#upload_form').addEventListener('submit', function(e){
+            e.preventDefault();
+            var file = document.getElementById('file').files[0];
+            api.upload(file, function(err, file){
+                if (err) return alert(err);
+                console.log("Stored in DB");
+            });
+            document.querySelector('#upload_form').reset();
+        });
+
+    });
 })();
