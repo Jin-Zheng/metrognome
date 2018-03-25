6616122fc8abf9b57a6ac170f25be5a9fa355635
Dennis T <den_tra1@hotmail.com>
2018-03-24 22:44:16 -0400

    Update app.js
    Fix bug in app.js

--- app.js
+++ app.js
@@ -397,7 +397,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 
-http.createServer(config, app).listen(PORT, function (err) {
+http.createServer(app).listen(PORT, function (err) {
     if (err) console.log(err);
     else console.log("HTTP server on http://localhost:%s", PORT);
 });
